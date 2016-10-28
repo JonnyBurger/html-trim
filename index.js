a960ade91729f4db271ae532fde047a19cdc2a8c
Jonny Burger <jonathanburger11@gmail.com>
2016-10-28 14:24:59 +0200

    uglifyjs compatible

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = input => {
+module.exports = function(input) {
 	input = input.replace(/&nbsp;/gi, ' ');
 	input = input.replace(/\s{2,}/gi, ' ');
 	input = input.replace(/(\s*<br\s*\/?>\s*){2,}/gi, '<br>');
